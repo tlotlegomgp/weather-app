a7966d3df1ddd83c892d2c06a4bbc59f1e70c479
Tlotlego <tlotlegomgp@gmail.com>
2021-02-12 13:25:11 +0200

    Updated the styling

--- index/forms.py
+++ index/forms.py
@@ -1,4 +1,4 @@
 from django import forms
 
 class SearchForm(forms.Form):
-	city_search = forms.CharField(max_length = 35, widget=forms.TextInput(attrs={'class':"form-control", 'name':"city_search", 'required': True, 'placeholder':"Enter city name", 'style':'border-color: #243E36;'}))
+	city_search = forms.CharField(max_length = 35, widget=forms.TextInput(attrs={'class':"form-control", 'name':"city_search", 'required': True, 'placeholder':"Enter city name", 'style':'border: 1px solid #243E36;'}))
