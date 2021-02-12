8b6046bcca5b1ed3c60cec109392663b106dd76a
Tlotlego <tlotlegomgp@gmail.com>
2021-02-12 13:08:46 +0200

    Changed Allowed hosts list

--- weatherApp/settings.py
+++ weatherApp/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('DJANGO_APP_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG_VALUE', ' ') == 'True'
 
-ALLOWED_HOSTS = ['4cast.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['blue-ski.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
